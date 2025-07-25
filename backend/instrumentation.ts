5190ef91e4e0b3d48917bc1b048afd1bfd197382
Eshietly <67698120+EshietIy@users.noreply.github.com>
2025-07-25 22:50:37 +0100

    fixbug

--- backend/instrumentation.ts
+++ backend/instrumentation.ts
@@ -1,30 +1,17 @@
+/*instrumentation.ts*/
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
-import { Resource } from '@opentelemetry/resources';
-import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+
 
 const sdk = new NodeSDK({
-  resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: 'user-service',
-    [SemanticResourceAttributes.SERVICE_VERSION]: '1.0',
-  }),
-  traceExporter: new OTLPTraceExporter({
+  traceExporter: new new OTLPTraceExporter({
     // optional - default url is http://localhost:4318/v1/traces
-    url: 'http://jaeger:4318/v1/traces',
+    url: 'http:jaeger/v1/traces',
+    // optional - collection of custom headers to be sent with each request, empty by default
     headers: {},
   }),
-
+  instrumentations: [getNodeAutoInstrumentations()],
 });
 
-// Start the SDK before your app starts
-sdk.start()
-  .then(() => console.log('OpenTelemetry SDK started'))
-  .catch((err) => console.error('Error starting OpenTelemetry SDK', err));
-
-// Optional: handle graceful shutdown
-process.on('SIGTERM', () => {
-  sdk.shutdown()
-    .then(() => console.log('OpenTelemetry SDK shutdown complete'))
-    .catch((err) => console.error('Error during OpenTelemetry SDK shutdown', err))
-    .finally(() => process.exit(0));
-});
+sdk.start();
