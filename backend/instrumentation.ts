4d51f62c8739d2c4c2cea42284f421d804bf7c61
Eshietly <67698120+EshietIy@users.noreply.github.com>
2025-07-26 10:38:57 +0100

    deploy to docker

--- backend/instrumentation.ts
+++ backend/instrumentation.ts
@@ -1,13 +1,13 @@
 /*instrumentation.ts*/
 import { NodeSDK } from '@opentelemetry/sdk-node';
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 
 
 const sdk = new NodeSDK({
   traceExporter: new OTLPTraceExporter({
     // optional - default url is http://localhost:4318/v1/traces
-    url: 'http:jaeger/v1/traces',
+    url: 'http:jaeger:4318/v1/traces',
     // optional - collection of custom headers to be sent with each request, empty by default
     headers: {},
   }),
